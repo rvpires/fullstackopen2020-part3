3c135bac81b13850f033679e567d1aee74df52f1
rvp <rodrigovazpires@hotmail.com>
2020-04-21 14:51:47 +0100

    added build for exercise 3.21

---
+++ build/precache-manifest.597ae61e448e36800e3dbdff175309bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371a9f53837f78d2f611b66b470e6fe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d7603599b4f23b387b5",
+    "url": "/static/css/main.24689720.chunk.css"
+  },
+  {
+    "revision": "f5ffb0dc48ad9952bcfd",
+    "url": "/static/js/2.5c68fe71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c68fe71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7603599b4f23b387b5",
+    "url": "/static/js/main.a6f24118.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
